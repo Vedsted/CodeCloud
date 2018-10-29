bc3be90e69e6ffa2fd5885bde897b1e15ebf7d7f
Jonas Vedsted Sørensen <jonso16@student.sdu.dk>
2018-10-29 15:35:19 +0100

    lines added to front

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,4 +1,28 @@
 
 function speak(): string {
     return "Hello World";
-} 
+}
+
+let num = 1;
+
+document.addEventListener('keypress', function (event) {
+    if (event.keyCode !== 13) {
+        return;
+    }
+    let line = document.createElement('div');
+
+    num++;
+
+    let l = document.createElement('label');
+    l.innerHTML = num + '. ';
+
+    let i = document.createElement('input');
+
+
+
+    line.appendChild(l);
+    line.appendChild(i);
+
+    document.body.appendChild(line);
+    i.focus();
+})
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,8 +5,10 @@ const http = require('http').Server(app);
 const socket = require('socket.io')(http);
 
 const port: number = 8080;
+
 app.use('/client', express.static(path.join(__dirname, '../client')));
 app.use('/css', express.static(path.join(__dirname, '../../webcontent/css')));
+
 
 app.get('/', function (req: any, res: any) {
     res.sendFile(path.join(__dirname, '../../webcontent/html/index.html'));
