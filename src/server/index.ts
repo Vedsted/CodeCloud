5dc72605470350c05e345608a53e00a8720072f6
Daniel Johansen <dajoh16@student.sdu.dk>
2018-10-29 14:20:20 +0100

    A Working website that routes correctly

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,18 @@
 const express = require('express');
-const socket = require('socketio');
 const path = require('path');
 const app = express()
+const http = require('http').Server(app);
+const socket = require('socket.io')(http);
 
 const port: number = 8080;
+app.use('/client', express.static(path.join(__dirname, '../client')));
+app.use('/css', express.static(path.join(__dirname, '../../webcontent/css')));
 
 app.get('/', function (req: any, res: any) {
     res.sendFile(path.join(__dirname, '../../webcontent/html/index.html'));
 });
 
-app.listen(port, function () {
+
+http.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 })
